b3a8419de9abdd5f9d3a5020710c14091162a08b
Fatih Mahawisesa <222011585@stis.ac.id>
2025-06-22 18:34:33 +0800

    malas

--- app/Filament/Pages/MasterTemplatePage.php
+++ app/Filament/Pages/MasterTemplatePage.php
@@ -23,4 +23,58 @@ class MasterTemplatePage extends Page
             'detailProgress' => $template ? $template->detail_progress : [],
         ];
     }
+
+    public array $detailProgress = [];
+    public bool $showModal = false;
+    protected $listeners = ['openModalMasterTemplateForm' => 'openModal'];
+
+    public function openModal()
+    {
+        $this->showModal = true;
+    }
+
+    public function closeModal()
+    {
+        $this->showModal = false;
+    }
+
+    public function mount()
+    {
+        $template = MasterTemplate::first();
+        $this->detailProgress = $template?->detail_progress ?? [];
+    }
+
+    public function addIndikator()
+    {
+        $this->detailProgress[] = [
+            'indikator' => '',
+            'detail' => [],
+        ];
+    }
+
+    public function removeIndikator($index)
+    {
+        unset($this->detailProgress[$index]);
+        $this->detailProgress = array_values($this->detailProgress);
+    }
+
+    public function addDetail($indikatorIndex)
+    {
+        $this->detailProgress[$indikatorIndex]['detail'][] = ['nama' => ''];
+    }
+
+    public function removeDetail($indikatorIndex, $detailIndex)
+    {
+        unset($this->detailProgress[$indikatorIndex]['detail'][$detailIndex]);
+        $this->detailProgress[$indikatorIndex]['detail'] = array_values($this->detailProgress[$indikatorIndex]['detail']);
+    }
+
+    public function save()
+    {
+        MasterTemplate::updateOrCreate(
+            ['id' => 1],
+            ['detail_progress' => $this->detailProgress]
+        );
+        session()->flash('message', 'Template berhasil disimpan!');
+    }
 }
--- resources/views/filament/pages/master-template.blade.php
+++ resources/views/filament/pages/master-template.blade.php
@@ -1,9 +1,69 @@
 <x-filament-panels::page>
 
-    <div class="space-y-6 border border-red-400">
+    <x-filament::modal>
+
+        <x-slot name="trigger">
+            <x-filament::button>
+                Ubah Indikator
+            </x-filament::button>
+        </x-slot>
+
+        <x-slot name="heading">
+            Ubah Daftar Indikator
+        </x-slot>
+
+        <div class="space-y-4">
+
+            <x-filament::button color="primary" wire:click="addIndikator"> + Tambah Indikator</x-filament::button>
+            <x-filament::button color="success" wire:click="save"> Simpan</x-filament::button>
 
 
-        <livewire:master-template-form />
+            @foreach ($detailProgress as $i => $indikator)
+                <x-filament::card>
+                    <label class="items-center justify-between block w-full mb-2 text-sm font-medium text-gray-700">
+                        Nama Indikator
+                        <div class="flex w-full gap-2 mt-1">
+                            <input wire:model="detailProgress.{{ $i }}.indikator"
+                                class="flex-grow p-2 border rounded" placeholder="Nama Indikator">
+                            <x-filament::button color="danger" size="sm"
+                                wire:click="removeIndikator({{ $i }})" class="p-2">
+                                <x-heroicon-o-trash class="w-4 h-4" />
+                            </x-filament::button>
+                        </div>
+                    </label>
+
+                    <label
+                        class="items-center justify-between block w-full mb-2 space-y-2 text-sm font-medium text-gray-700">
+                        Detail Indikator
+                        @foreach ($indikator['detail'] as $j => $item)
+                            <div class="flex gap-2">
+                                <input wire:model="detailProgress.{{ $i }}.detail.{{ $j }}.nama"
+                                    class="w-full p-2 border rounded" placeholder="Nama Detail">
+                                <x-filament::button size="sm" color="danger"
+                                    wire:click="removeDetail({{ $i }}, {{ $j }})">
+                                    <x-heroicon-o-trash class="w-4 h-4" />
+                                </x-filament::button>
+                            </div>
+                        @endforeach
+                    </label>
+
+                    <div class="mt-2">
+                        <x-filament::button size="sm" wire:click="addDetail({{ $i }})">
+                            + Tambah Detail Indikator
+                        </x-filament::button>
+                    </div>
+
+                </x-filament::card>
+            @endforeach
+
+            @if (session()->has('message'))
+                <div class="text-green-600">{{ session('message') }}</div>
+            @endif
+        </div>
+
+    </x-filament::modal>
+
+    <div class="space-y-6 ">
 
         <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
             @foreach ($detailProgress as $indikator)
@@ -17,9 +77,6 @@
                 </x-filament::card>
             @endforeach
         </div>
-
-
-
     </div>
 
 </x-filament-panels::page>
